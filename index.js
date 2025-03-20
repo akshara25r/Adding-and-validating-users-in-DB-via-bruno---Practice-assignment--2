6c338821a29fad980a86596fa649119d4474b03f
akshara25r <aksharar02@gmail.com>
2025-03-20 10:05:02 +0530

    complete

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth.js');  // Import the auth routes
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+// MongoDB connection
+mongoose.connect('mongodb+srv://akshara:Aksharad6@cluster0.2uks2.mongodb.net/demo-assignment')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(error));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Use the auth routes
+app.use('/api', authRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
